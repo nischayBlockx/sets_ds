46a399af8d9bd6d85bb081cd015b0e82b96d901c
Nischay Vaish <nischay.vaish@gmail.com>
2023-10-26 11:22:34 +0530

    Update deploy.ts

--- kunji_finance/scripts/deploy.ts
+++ kunji_finance/scripts/deploy.ts
@@ -8,6 +8,10 @@ async function deploy() {
   const contract = await SetsFactory.deploy(5)
   await contract.waitForDeployment();
 
+  const deploymentDelaySeconds = 30;
+  console.log(`Waiting for ${deploymentDelaySeconds} seconds...`);
+  await new Promise(resolve => setTimeout(resolve, deploymentDelaySeconds * 1000));
+
 
   const contractAddress = contract.target;
   console.log("Contract deployed to:", contractAddress);
